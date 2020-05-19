59d95fff426fd17063de984452b3d22807145fb9
stostat <santmendieta@icloud.com>
2020-05-18 19:47:27 -0500

    fix except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
-            print(my_list[i], end='')
+            print("{}".format(my_list[i]), end='')
             i += 1
         except IndexError:
-            i -= 1
             pass
     print()
     return i
