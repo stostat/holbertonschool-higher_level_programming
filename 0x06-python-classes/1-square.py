b16b39c0c4c1562865cac266effe3179a3e2a32e
stostat <santmendieta@icloud.com>
2020-05-19 20:31:40 -0500

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class defining a square."""
+
+
+class Square:
+    """class defining a Square."""
+
+    def __init__(self, size):
+        """Initialize private attributes."""
+        self.__size = size
