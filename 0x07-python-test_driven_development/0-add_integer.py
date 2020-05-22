8df0e82d2a88865e3c5149efa25aaeb75d07e98a
stostat <santmendieta@icloud.com>
2020-05-22 16:32:57 -0500

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Add two integers."""
+
+
+def add_integer(a, b=98):
+    """Add two integers."""
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+DOCTESTS AND UNIT TESTS
+
+we will learn to use bith for testing, creating our own checkers
