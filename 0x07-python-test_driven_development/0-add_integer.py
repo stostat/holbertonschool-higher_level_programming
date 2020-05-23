2effe498596240ad4b9b241abdff0996df4772c5
stostat <santmendieta@icloud.com>
2020-05-22 21:29:46 -0500

    second commit, tests with 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,15 @@
 """Add two integers."""
 
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+
 def add_integer(a, b=98):
     """Add two integers."""
-    return a + b
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a + b)
