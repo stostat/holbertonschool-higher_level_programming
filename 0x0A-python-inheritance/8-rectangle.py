b98233eaed42a69f90b89e865f00db794cbce0d4
stostat <santmendieta@icloud.com>
2020-06-02 23:57:04 -0500

    correction in name

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):
     """Raise exception."""
 
     def __init__(self, width, height):
