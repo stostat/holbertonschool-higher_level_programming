e9d34e3df0671c06cbae61e16031d56f0a305317
stostat <santmendieta@icloud.com>
2020-06-02 23:20:34 -0500

    6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empty class."""
+
+
+class BaseGeometry:
+    """Raise exception."""
+
+    def area(self):
+        """Raise exception."""
+        raise Exception("area() is not implemented")
