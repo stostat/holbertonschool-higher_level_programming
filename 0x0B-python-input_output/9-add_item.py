9c5ac80d6cfbc812c46dc035ad5bc51cfdeed221
stostat <santmendieta@icloud.com>
2020-06-04 18:59:15 -0500

    9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Program that adds item to json file."""
-
+"""Script that adds item to json file."""
 import sys
+import json
 from os import path
+
 save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
 load_from_json_f = __import__('8-load_from_json_file.py').load_from_json_file
 
 file = "add_item.json"
-
 if path.exists(file):
     list_args = load_from_json_f(file)
 else:
