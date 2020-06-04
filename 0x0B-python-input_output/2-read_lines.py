1e064f63e3146bbbd2a489f561d67ad9878c5c02
stostat <santmendieta@icloud.com>
2020-06-03 21:45:09 -0500

    readlines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Program that opens and counts  a file's lines using with."""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Read a file and coubts its lines.
+
+    Args: filename- file to open
+    return: Conter
+    """
+    i = 0
+    with open(filename) as f:
+        if nb_lines == 0:
+            print(f.read())
+        else:
+            while i <= nb_lines:
+                print(f.readline(), end='')
+                i += 1
