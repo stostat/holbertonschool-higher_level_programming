d567f51e435392652427174dbb34fb41bfc7a1b4
stostat <santmendieta@icloud.com>
2020-06-04 18:53:43 -0500

    9

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Program that loads json file to object in python."""
+import sys
+from os import path
+save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
+load_from_json_f = __import__('8-load_from_json_file.py').load_from_json_file
+
+file = "add_item.json"
+
+if path.exists(file):
+    list_args = load_from_json_f(file)
+else:
+    list_args = []
+
+if len(sys.argv) > 1:
+    for i in sys.argv[1:]:
+        list_args.append(i)
+
+save_to_json_file(list_args, file)
