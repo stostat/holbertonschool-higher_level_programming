af23ce27573935cc7f012de6371c01a31bee591a
stostat <santmendieta@icloud.com>
2020-06-04 19:05:15 -0500

    9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Script that adds item to json file."""
-
 import sys
-import json
-import os.path as check
+
 
 save_to_json_file = __import__('7-save_to_json_file.py').save_to_json_file
 load_from_json_f = __import__('8-load_from_json_file.py').load_from_json_file
 
 file = "add_item.json"
-if check.exists(file):
-    list_args = load_from_json_f(file)
-else:
-    list_args = []
-
-if len(sys.argv) > 1:
+try:
+    pl = load_from_json_f(file)
+except Exception:
+    pl = []
+finally:
     for i in sys.argv[1:]:
-        list_args.append(i)
-
-save_to_json_file(list_args, file)
+        pl.append(i)
+    save_to_json_file(pl, file)
