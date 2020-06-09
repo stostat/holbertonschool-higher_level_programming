89850e7418ff686a973716774853b39a84e23451
stostat <santmendieta@icloud.com>
2020-06-09 18:05:51 -0500

    to dictionary squares documentation corected

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """First base module."""
+import json
 
 
 class Base:
@@ -14,3 +15,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Take Dictionary to json."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
