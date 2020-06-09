be3e3c9df1e3423455590f9da29ca6bdcba581f0
stostat <santmendieta@icloud.com>
2020-06-09 14:49:57 -0500

    documentation corrected

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,29 @@ class Square(Rectangle):
         """Initialize attributes in class."""
         super().__init__(size, size, x, y, id)
         self.size = size
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Override of the str method."""
+        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                    self.__y, self.__width)
+
+    def update(self, *args, **kwargs):
+        """Update that receives args."""
+        measures = ['id', 'width', 'x', 'y']
+        if args is not None and len(args) != 0:
+            for i in range(len(args)):
+                setattr(self, measures[i], args[i])
+        else:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
