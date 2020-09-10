7b60a0df2c64a89a307e8339860af64020e886ef
root <santmendieta@icloud.com>
2020-09-10 13:10:26 -0500

    Multiply list map name corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     """
-    :param roman_string:
-    :return: te integer value of a roman number in arabic decimal
+    param roman_string:
+    return: te integer value of a roman number in arabic decimal
     """
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_sum = 0
