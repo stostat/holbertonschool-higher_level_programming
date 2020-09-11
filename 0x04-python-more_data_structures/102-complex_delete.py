17cff73286281e01d22d706def782e06a923f08e
stostat <santmendieta@icloud.com>
2020-09-11 00:03:07 -0500

    feature

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return dict(filter(lambda elem: elem[1] == value, a_dictionary.values()))
+    return dict(filter(lambda elem: elem[1] == value, a_dictionary.items()))
