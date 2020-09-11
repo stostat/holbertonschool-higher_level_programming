22be28546401730bd1493e3ac37c73af7a8c7f1b
stostat <santmendieta@icloud.com>
2020-09-10 21:01:48 -0500

    pep8 fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    """
+    param roman_string:
+    return: te integer value of a roman number in arabic decimal
+    """
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                 'M': 1000}
     num_sum = 0
     if roman_string is None or isinstance(roman_string, str) is False:
         return 0
