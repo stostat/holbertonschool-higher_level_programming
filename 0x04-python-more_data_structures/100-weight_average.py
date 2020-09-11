ff05bf48aee41f4ad50142d6d39825d025685a1f
stostat <santmendieta@icloud.com>
2020-09-10 21:36:04 -0500

    Feature - advanced 100, pycode style correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,5 @@
 def weight_average(my_list=[]):
     if my_list == []:
         return 0
-    return sum(map(lambda x: int(x[0] * x[1]), my_list)) / sum(map(lambda x: int(x[1]), my_list))
+    avg = sum(map(lambda x: int(x[0] * x[1]), my_list))
+    return avg / sum(map(lambda x: int(x[1]), my_list))
