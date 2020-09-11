d3de85e6eaa127ff0f474216acd2c4cf76dc9ccb
stostat <santmendieta@icloud.com>
2020-09-10 20:54:36 -0500

    roman numbers corrected

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    """
-    param roman_string:
-    return: te integer value of a roman number in arabic decimal
-    """
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num_sum = 0
     if roman_string is None or isinstance(roman_string, str) is False:
