7237366f5ae88fb6069801275b88158f8e036981
root <santmendieta@icloud.com>
2020-11-18 21:49:56 -0500

    first commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- User creation and privileges shown
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+In this project we will work with users and privileges, joint tables and more
