3def423899439eb2d455f3e218823ccc96b985da
root <santmendieta@icloud.com>
2020-11-18 21:52:20 -0500

    comment line added

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- User creation and privileges shown
+-- user creation and privileges shown
+-- for two different users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
