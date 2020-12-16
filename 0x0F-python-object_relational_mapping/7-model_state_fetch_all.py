1f0d5929345e36f06a73698dd04b8fd42fec496c
root <santmendieta@icloud.com>
2020-12-16 17:07:00 -0500

    7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Start link class to table in database"""
 
 import sys
 from sqlalchemy import (create_engine)
@@ -9,7 +8,9 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True, echo=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     session = Session(bind=engine)
     query = session.query(State)
 
