79868e893feae43f0df7f643f81997de0588ac21
root <santmendieta@icloud.com>
2020-12-19 00:01:34 -0500

    correction

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base model of a City."""
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """Class for city """
+    __tablename__ = "cities"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer,ForeignKey("states.id"), nullable=False)
